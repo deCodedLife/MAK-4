6ee3b49d004f9bf0d7d598df2c7f5f3de2a26d8b
CodedLife <dcodedlife@gmail.com>
2023-10-17 17:03:26 +0300

    Initial commit

---
+++ resources/qml/Main.qml
@@ -0,0 +1,14 @@
+import QtQuick
+import QtQuick.Controls
+import QtQuick.Controls.Material
+
+ApplicationWindow
+{
+    width: 800
+    height: 600
+
+    visible: true
+    title: "MAK-4"
+
+    Material.accent: Material.Teal
+}
---
+++ sources/main.cpp
@@ -0,0 +1,11 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/qml/Main.qml")));
+    return app.exec();
+}
