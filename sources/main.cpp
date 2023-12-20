bdc32df29aef9f1ffe4a764e2bd685227ed18d71
deCodedLife <deCodedLife@5957f1d8-9f45-11ee-8bcb-3935135b4db4>
2023-12-20 20:36:12 +0000

    Add snmp and openssl libs
    git-svn-id: svn://redmine.promsd.ru:39600/sw-mak4-snmp/trunk@6 5957f1d8-9f45-11ee-8bcb-3935135b4db4

--- sources/main.cpp
+++ sources/main.cpp
@@ -3,6 +3,11 @@
 
 #include <configs.h>
 
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/net-snmp-features.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
